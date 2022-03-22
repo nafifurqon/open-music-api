73acf3270c9b7200f6e75601402c5504df759c90
nafifurqon <muhammadnafi98@gmail.com>
2022-03-22 22:34:58 +0700

    feat: create add album api

---
+++ src/api/albums/handler.js
@@ -0,0 +1,48 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+      const { name, year } = request.payload;
+
+      const albumId = await this._service.addAlbum({ name, year });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Catatan berhasil ditambahkan',
+        data: {
+          albumId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(500);
+      return response;
+    }
+  }
+}
+
+module.exports = AlbumsHandler;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -10,6 +15,14 @@ const init = async () => {
       cors: {
         origin: ['*'],
       },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
     },
   });
 
