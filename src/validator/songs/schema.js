2930aa06c16825e683d750d04ca15fb8d4bb8905
nafifurqon <muhammadnafi98@gmail.com>
2022-03-26 10:57:59 +0700

    fix: year validation

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
 
+const currentYear = new Date().getFullYear();
+
 const AlbumPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().min(1).max(currentYear).required(),
   genre: Joi.string().required(),
   performer: Joi.string().required(),
   duration: Joi.number(),
