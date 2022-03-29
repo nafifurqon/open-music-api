a82d66ddfdca3f1ea654cbe5f83e2c1c15fdf551
nafifurqon <muhammadnafi98@gmail.com>
2022-03-29 07:17:35 +0700

    feat: set null albumId on songs to old_albums and rename albumId to be albumid

--- migrations/1648357590316_set-null-albumId-as-old-albums.js
+++ migrations/1648357590316_set-null-albumId-as-old-albums.js
@@ -11,21 +11,10 @@ exports.up = (pgm) => {
   );
 
   pgm.sql('UPDATE songs SET "albumId" = \'old_albums\' WHERE "albumId" is NULL');
-
-  pgm.alterColumn('songs', 'albumId', {
-    notNull: true,
-  });
 };
 
 exports.down = (pgm) => {
-  pgm.alterColumn('songs', 'albumId', {
-    notNull: false,
-  });
-
   pgm.sql('UPDATE songs SET "albumId" = NULL WHERE "albumId" = \'old_albums\'');
 
-  pgm.sql(
-    'INSERT INTO albums(id, name, year, created_at, updated_at) '
-    + `VALUES ('old_albums', 'old_albums', 2022, ${now}, ${now})`,
-  );
+  pgm.sql('DELETE FROM albums WHERE id = \'old_albums\'');
 };
---
+++ migrations/1648511241284_rename-column-albumId-to-be-albumid-on-songs-table.js
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.renameColumn('songs', 'albumId', 'albumid');
+};
+
+exports.down = (pgm) => {
+  pgm.renameColumn('songs', 'albumid', 'albumId');
+};
