923f36146b9bbc3e89b7658f6fbf1c370a6f0e20
nafifurqon <muhammadnafi98@gmail.com>
2022-04-07 09:20:26 +0700

    chore: create user_album_like table migration

---
+++ migrations/1649295501530_create-table-user-album-likes.js
@@ -0,0 +1,32 @@
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'cascade',
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'albums',
+      onDelete: 'cascade',
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('user_album_likes');
+};
